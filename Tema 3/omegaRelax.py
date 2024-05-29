88667d8e6891c55d8746120459196ce680635b66
tdemagneval <135025049+tdemagneval@users.noreply.github.com>
2024-05-29 12:38:50 +0200

    Update omegaRelax.py

--- Tema 3/omegaRelax.py
+++ Tema 3/omegaRelax.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def iterRel (w, x0, matA, b, tol=1.e-10, maxIter=100):
+def iterRel (w, z, matA, b, tol=1.e-10, maxIter=100):
+    matA, b, x0 = np.array(matA, dtype = float), np.array(b, dtype = float), np.array(z, dtype = float)
     n = len(b)
     x = x0.copy()
     for k in range(maxIter):
@@ -8,9 +9,9 @@ def iterRel (w, x0, matA, b, tol=1.e-10, maxIter=100):
             x[i] = b[i]
             for j in range(n):
                 if (j < i): 
-                    x[i] -= matA[i,j]*x0[j]
+                    x[i] -= matA[i,j]*x[j]
                 if (j > i):
-                    x[i] -= matA[i,j]*x[j]      
+                    x[i] -= matA[i,j]*x0[j]      
             x[i] *= w/matA[i,i]
             x[i] += (1 - w)*x0[i]
         if (np.linalg.norm(x-x0) < tol*np.linalg.norm(x)):
